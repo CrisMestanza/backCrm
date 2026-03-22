b332280691aa8bb70cc0cfe00ef012fb4b68ef2d
Hzsxz <166932045+Hzsxz@users.noreply.github.com>
2026-03-22 14:07:43 -0500

    Ya

--- crm/settings.py
+++ crm/settings.py
@@ -11,11 +11,14 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import sys
 from pathlib import Path
+import os
+
+STATIC_URL = '/static/'
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
