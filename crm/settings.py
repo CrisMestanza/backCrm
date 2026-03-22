00b72760883cdcc2acaceba458fbea9a44a400c7
Hzsxz <166932045+Hzsxz@users.noreply.github.com>
2026-03-22 12:42:41 -0500

    deploy5

--- crm/settings.py
+++ crm/settings.py
@@ -9,7 +9,12 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
+import sys
+import pymysql
 
+pymysql.install_as_MySQLdb()
+
+sys.modules['MySQLdb'] = pymysql
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
